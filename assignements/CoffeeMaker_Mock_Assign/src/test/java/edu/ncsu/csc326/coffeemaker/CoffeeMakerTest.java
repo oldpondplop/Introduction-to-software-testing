4ce218418033712b15550afca17706539d910496
ntixitho <ntixitho@protonmail.com>
2022-05-11 15:38:56 +0300

    Coffee Maker Asignement Stubs&Mocks

--- assignements/CoffeeMaker_Mock_Assign/src/test/java/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
+++ assignements/CoffeeMaker_Mock_Assign/src/test/java/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
@@ -17,8 +17,7 @@
  */
 package edu.ncsu.csc326.coffeemaker;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import edu.ncsu.csc326.coffeemaker.exceptions.InventoryException;
@@ -124,49 +123,57 @@ public class CoffeeMakerTest {
 
 		stubRecipies = new Recipe [] {r1, r2, r3};
 	}
-	
-	
+
+
 	//-----------------------------------------------------------------------
 	//	Test Methods
 	//-----------------------------------------------------------------------
-	
+
 	// put your tests here!
-	
+
 	@Test
 	public void testMakeCoffee() {
 		cm = new CoffeeMaker(recipeBookStub, new Inventory());
 		assertTrue(true);
 	}
+
 	//-----------------------------------------------------------------------
 	//	Purchase coffee with enough ingredients & money
 	//-----------------------------------------------------------------------
+
 	@Test
 	public void testPurchaseBeverage() {
 		when(recipeBookStub.getRecipes()).thenReturn(stubRecipies);
 		cm = new CoffeeMaker(recipeBookStub, new Inventory());
 		assertEquals(25, cm.makeCoffee(0,75));
 	}
+
 	//-----------------------------------------------------------------------
 	//	Not enough money
 	//-----------------------------------------------------------------------
+
 	@Test
 	public void testPurchaseBeverage1() {
 		when(recipeBookStub.getRecipes()).thenReturn(stubRecipies);
 		cm = new CoffeeMaker(recipeBookStub, new Inventory());
 		assertEquals(49, cm.makeCoffee(0,49));
 	}
+
 	//-----------------------------------------------------------------------
 	//	Not enough ingredients
 	//-----------------------------------------------------------------------
+
 	@Test
 	public void testPurchaseBeverage2() {
 		when(recipeBookStub.getRecipes()).thenReturn(stubRecipies);
 		cm = new CoffeeMaker(recipeBookStub, new Inventory());
 		assertEquals(100, cm.makeCoffee(1,100));
 	}
+
 	//-----------------------------------------------------------------------
 	//	Exact amount of money
 	//-----------------------------------------------------------------------
+
 	@Test
 	public void testPurchaseBeverage3() {
 		when(recipeBookStub.getRecipes()).thenReturn(stubRecipies);
@@ -178,37 +185,63 @@ public class CoffeeMakerTest {
 	//-----------------------------------------------------------------------
 	//	Trying to purchase a beverage that doesn't exist
 	//-----------------------------------------------------------------------
+
 	@Test
 	public void testPurchaseBeverage4() {
 		when(recipeBookStub.getRecipes()).thenReturn(new Recipe[]{r1,r2,null});
 		cm = new CoffeeMaker(recipeBookStub, new Inventory());
 		assertEquals(100, cm.makeCoffee(2,100));
 	}
+
+	//-----------------------------------------------------------------------
+	//	Trying to purchase a beverage at index that doesn't exist
+	//-----------------------------------------------------------------------
+
+	@Test
+	public void testPurchaseBeverage5() {
+		when(recipeBookStub.getRecipes()).thenReturn(new Recipe[]{r1,r2,r5});
+		cm = new CoffeeMaker(recipeBookStub, new Inventory());
+		assertEquals(100, cm.makeCoffee(4,100));
+	}
+
 	//-----------------------------------------------------------------------
 	// verify that inventory.useIngredients gets called
 	// -----------------------------------------------------------------------
-	@Test
-	public void testPurchaseBeverage5() {
-		Inventory I = spy(new Inventory());
-		when(recipeBookStub.getRecipes()).thenReturn(stubRecipies);
-		cm=new CoffeeMaker(recipeBookStub, I);
-		cm.makeCoffee(1, 100);
-		verify(I).useIngredients(r2);
-	}
-	//-----------------------------------------------------------------------
-	//	verify that inventory.enoughIngredients gets called
-	// -----------------------------------------------------------------------
+
 	@Test
 	public void testPurchaseBeverage6() {
 		Inventory I = spy(new Inventory());
-		when(recipeBookStub.getRecipes()).thenReturn(stubRecipies);
+
+		when(recipeBookStub.getRecipes()).thenReturn(new Recipe[]{r1,r3,r5});
+
 		cm=new CoffeeMaker(recipeBookStub, I);
+
 		cm.makeCoffee(1, 100);
-		verify(I).enoughIngredients(r2);
+
+		verify(I).useIngredients(r3);
 	}
+
+	//-----------------------------------------------------------------------
+	//	verify that inventory.enoughIngredients gets called
+	// -----------------------------------------------------------------------
+
+	@Test
+	public void testPurchaseBeverage7() {
+		Inventory I = spy(new Inventory());
+
+		when(recipeBookStub.getRecipes()).thenReturn(new Recipe[]{r1,r3,r5});
+
+		cm=new CoffeeMaker(recipeBookStub, I);
+
+		cm.makeCoffee(1, 100);
+
+		verify(I).enoughIngredients(r3);
+	}
+
 	//-----------------------------------------------------------------------
 	//	verify that recipe methods are called once for the selected recipe and not for the other recipes
 	// -----------------------------------------------------------------------
+
 	@Test
 	public void testCallRecipeMethods() {
 		Recipe spy_r1 = spy(r1);
@@ -240,46 +273,78 @@ public class CoffeeMakerTest {
 		verify(spy_r3, times(0)).getPrice();
 
 	}
-
-
-	@Test(expected = InventoryException.class)
-	public void testAddInventoryException() throws InventoryException {
-		cm.addInventory("4", "1", "asdf", "3");
-	}
-	@Test(expected = InventoryException.class)
-	public void testAddInventoryException1() throws InventoryException {
-		cm.addInventory("1", "asdf", "1", "1");
-	}
-	@Test(expected = InventoryException.class)
-	public void testAddInventoryException2() throws InventoryException {
-		cm.addInventory("asdf", "1", "1", "1");
-	}
-	@Test(expected = InventoryException.class)
-	public void testAddInventoryException3() throws InventoryException {
-		cm.addInventory("1", "1", "1", "asdf");
-	}
-	@Test(expected = InventoryException.class)
-	public void testAddInventoryException4() throws InventoryException {
-		cm.addInventory("-1", "1", "1", "1");
-	}
-	@Test(expected = InventoryException.class)
-	public void testAddInventoryException5() throws InventoryException {
-		cm.addInventory("1", "-1", "1", "1");
-	}
-	@Test(expected = InventoryException.class)
-	public void testAddInventoryException6() throws InventoryException {
-		cm.addInventory("1", "1", "-1", "1");
-	}
-	@Test(expected = InventoryException.class)
-	public void testAddInventoryException7() throws InventoryException {
-		cm.addInventory("1", "1", "1", "-1");
-	}
-
+	//-----------------------------------------------------------------------
+	//	verify that recipe methods are called once for the selected recipe and not for the other recipes
+	// -----------------------------------------------------------------------
 	@Test
-	public void testAddInventory() throws InventoryException {
-		cm.addInventory("4","7","0","9");
+	public void testCallRecipeMethods1() {
+
+		Recipe spy_r3 = spy(r3);
+		Recipe spy_r4 = spy(r4);
+		Recipe spy_r5 = spy(r5);
+
+		when(recipeBookStub.getRecipes()).thenReturn(new Recipe[]{spy_r3,spy_r4,spy_r5});
+		cm = new CoffeeMaker(recipeBookStub, new Inventory());
+
+		assertEquals(100,cm.makeCoffee(2,200));
+
+		verify(spy_r3, times(0)).getAmtCoffee();
+		verify(spy_r3, times(0)).getAmtMilk();
+		verify(spy_r3, times(0)).getAmtSugar();
+		verify(spy_r3, times(0)).getAmtChocolate();
+		verify(spy_r3, times(0)).getPrice();
+
+		verify(spy_r4, times(0)).getAmtCoffee();
+		verify(spy_r4, times(0)).getAmtMilk();
+		verify(spy_r4, times(0)).getAmtSugar();
+		verify(spy_r4, times(0)).getAmtChocolate();
+		verify(spy_r4, times(0)).getPrice();
+
+		verify(spy_r5, atLeastOnce()).getAmtCoffee();
+		verify(spy_r5, atLeastOnce()).getAmtMilk();
+		verify(spy_r5, atLeastOnce()).getAmtSugar();
+		verify(spy_r5, atLeastOnce()).getAmtChocolate();
+		verify(spy_r5, atLeastOnce()).getPrice();
+	}
+	//-----------------------------------------------------------------------
+	//	verify that recipe methods are called once for the selected recipe and not for the other recipes
+	// -----------------------------------------------------------------------
+	@Test
+	public void testCallRecipeMethods2() {
+
+		Recipe spy_r3 = spy(r3);
+		Recipe spy_r2 = spy(r2);
+		Recipe spy_r4 = spy(r4);
+
+		when(recipeBookStub.getRecipes()).thenReturn(new Recipe[]{spy_r3,spy_r4,spy_r2});
+		cm = new CoffeeMaker(recipeBookStub, new Inventory());
+
+		assertEquals(135,cm.makeCoffee(1,200));
+
+		verify(spy_r4, atLeast(1)).getAmtCoffee();
+		verify(spy_r4, atLeast(1)).getAmtMilk();
+		verify(spy_r4, atLeast(1)).getAmtSugar();
+		verify(spy_r4, atLeast(1)).getAmtChocolate();
+		verify(spy_r4, atLeast(1)).getPrice();
+
+		verify(spy_r3, times(0)).getAmtCoffee();
+		verify(spy_r3, times(0)).getAmtMilk();
+		verify(spy_r3, times(0)).getAmtSugar();
+		verify(spy_r3, times(0)).getAmtChocolate();
+		verify(spy_r3, times(0)).getPrice();
+
+		verify(spy_r2, times(0)).getAmtCoffee();
+		verify(spy_r2, times(0)).getAmtMilk();
+		verify(spy_r2, times(0)).getAmtSugar();
+		verify(spy_r2, times(0)).getAmtChocolate();
+		verify(spy_r2, times(0)).getPrice();
+
+
 	}
 
+	//-----------------------------------------------------------------------
+	//	check inventory
+	// -----------------------------------------------------------------------
 	@Test
 	public void testCheckInventory(){
 
@@ -303,223 +368,22 @@ public class CoffeeMakerTest {
 						cm.checkInventory());
 	}
 
+	//-----------------------------------------------------------------------
+	//	check inventory before and after purchase
+	// -----------------------------------------------------------------------
 	@Test
-	public void testDeleteRecipe() {
-		when(recipeBookStub.getRecipes()).thenReturn(new Recipe[]{r1, r2, r3});
-		cm = new CoffeeMaker(recipeBookStub, new Inventory());
-
-		when(recipeBookStub.deleteRecipe()).thenReturn(r1.getName());
-
-		String deleted_recipe = cm.deleteRecipe(0);
-
-		assertEquals(r1.getName(), deleted_recipe);
-	}
-
-	@Test
-	public void testMakeCoffeePayingMore() {
+	public void testCheckInventory2(){
 		when(recipeBookStub.getRecipes()).thenReturn(stubRecipies);
-		cm = new CoffeeMaker(recipeBookStub, new Inventory());
-		assertEquals(25, cm.makeCoffee(0, 75));
-	}
 
-	@Test
-	public void testMakeCoffeePayingLess()  {
-		when(recipeBookStub.getRecipes()).thenReturn(stubRecipies);
-		cm = new CoffeeMaker(recipeBookStub, new Inventory());
-		assertEquals(10, cm.makeCoffee(0, 10));
-	}
-
-	@Test
-	public void testMakeCoffeePayingExactly() {
-		when(recipeBookStub.getRecipes()).thenReturn(stubRecipies);
-		cm = new CoffeeMaker(recipeBookStub, new Inventory());
-		assertEquals(0, cm.makeCoffee(0, 50));
-	}
-
-	@Test
-	public void testMakeCoffeeInvalidPayment() {
-		when(recipeBookStub.getRecipes()).thenReturn(stubRecipies);
-		cm = new CoffeeMaker(recipeBookStub, new Inventory());
-		assertEquals(-1, cm.makeCoffee(0, -1));
-	}
-
-
-	@Test
-	public void testMakeCofeeMultipleRecipes() {
-		when(recipeBookStub.getRecipes()).thenReturn(stubRecipies);
-		cm = new CoffeeMaker(recipeBookStub, new Inventory());
-		assertEquals(0, cm.makeCoffee(2, 100));
-	}
-
-
-	@Test
-	public void testMakeCofeeInvalidRecipe2() {
-		when(recipeBookStub.getRecipes()).thenReturn(new Recipe[] {r1,r2,r3,r4,null});
-		cm = new CoffeeMaker(recipeBookStub, new Inventory());
-		assertEquals(100, cm.makeCoffee(4, 100));
-	}
-
-
-	@Test
-	public void testAddCoffee() throws InventoryException {
 		Inventory I = new Inventory();
-		I.addCoffee("10");
-		assertEquals(25, I.getCoffee());
-	}
+		cm=new CoffeeMaker(recipeBookStub, new Inventory());
 
-	@Test
-	public void testAddMilk() throws InventoryException {
-		Inventory I = new Inventory();
-		I.addMilk("10");
-		assertEquals(25, I.getMilk());
-	}
+		String initialInventory = cm.checkInventory();
+		cm.makeCoffee(0, 75);
+		String finalInventory = cm.checkInventory();
 
-	@Test
-	public void testAddSugar() throws InventoryException {
-		Inventory I = new Inventory();
-		I.addSugar("10");
-		assertEquals(25, I.getSugar());
-	}
+		assertNotEquals(initialInventory, finalInventory);
 
-	@Test
-	public void testAddChocolate() throws InventoryException {
-		Inventory I = new Inventory();
-		I.addChocolate("10");
-		assertEquals(25, I.getChocolate());
-	}
-
-	@Test(expected=RecipeException.class)
-	public void testCreateRecipe() throws RecipeException {
-		Recipe r = new Recipe();
-		r.setName("test");
-		r.setAmtCoffee("-1");
-		r.setAmtMilk("1");
-		r.setAmtSugar("1");
-		r.setAmtChocolate("1");
-		r.setPrice("1");
-	}
-
-	@Test(expected=RecipeException.class)
-	public void testCreateRecipe1() throws RecipeException {
-		Recipe r = new Recipe();
-		r.setName("test");
-		r.setAmtCoffee("1");
-		r.setAmtMilk("-1");
-		r.setAmtSugar("1");
-		r.setAmtChocolate("1");
-		r.setPrice("1");
-	}
-
-	@Test(expected=RecipeException.class)
-	public void testCreateRecipe2() throws RecipeException {
-		Recipe r = new Recipe();
-		r.setName("test");
-		r.setAmtCoffee("1");
-		r.setAmtMilk("1");
-		r.setAmtSugar("-1");
-		r.setAmtChocolate("1");
-		r.setPrice("1");
-	}
-	@Test(expected=RecipeException.class)
-	public void testCreateRecipe3() throws RecipeException {
-		Recipe r = new Recipe();
-		r.setName("test");
-		r.setAmtCoffee("1");
-		r.setAmtMilk("1");
-		r.setAmtSugar("1");
-		r.setAmtChocolate("-1");
-		r.setPrice("1");
-	}
-
-	@Test(expected=RecipeException.class)
-	public void testCreateRecipe4() throws RecipeException {
-		Recipe r = new Recipe();
-		r.setName("test");
-		r.setAmtCoffee("1");
-		r.setAmtMilk("1");
-		r.setAmtSugar("1");
-		r.setAmtChocolate("1");
-		r.setPrice("-1");
-	}
-
-	@Test(expected=RecipeException.class)
-	public void testCreateRecipe5() throws RecipeException {
-		Recipe r = new Recipe();
-		r.setName("test");
-		r.setAmtCoffee("a");
-		r.setAmtMilk("1");
-		r.setAmtSugar("2");
-		r.setAmtChocolate("3");
-		r.setPrice("4");
-	}
-
-	@Test(expected=RecipeException.class)
-	public void testCreateRecipe6() throws RecipeException {
-		Recipe r = new Recipe();
-		r.setName("test");
-		r.setAmtCoffee("1");
-		r.setAmtMilk("a");
-		r.setAmtSugar("2");
-		r.setAmtChocolate("3");
-		r.setPrice("4");
-	}
-	@Test(expected=RecipeException.class)
-	public void testCreateRecipe7() throws RecipeException {
-		Recipe r = new Recipe();
-		r.setName("test");
-		r.setAmtCoffee("1");
-		r.setAmtMilk("1");
-		r.setAmtSugar("a");
-		r.setAmtChocolate("3");
-		r.setPrice("4");
-	}
-	@Test(expected=RecipeException.class)
-	public void testCreateRecipe8() throws RecipeException {
-		Recipe r=new Recipe();
-		r.setName("test");
-		r.setAmtCoffee("1");
-		r.setAmtMilk("1");
-		r.setAmtSugar("1");
-		r.setAmtChocolate("a");
-		r.setPrice("4");
-	}
-	@Test(expected=RecipeException.class)
-	public void testCreateRecipe9() throws RecipeException {
-		Recipe r=new Recipe();
-		r.setName("test");
-		r.setAmtCoffee("1");
-		r.setAmtMilk("1");
-		r.setAmtSugar("1");
-		r.setAmtChocolate("1");
-		r.setPrice("a");
-	}
-	@Test(expected=RecipeException.class)
-	public void testCreateRecipe10() throws RecipeException {
-		Recipe r=new Recipe();
-		r.setName(null);
-		r.setAmtCoffee("1");
-		r.setAmtMilk("1");
-		r.setAmtSugar("1");
-		r.setAmtChocolate("1");
-		r.setPrice("a");
-
-	}
-
-
-	@Test
-	public void testCreateRecipe11() throws RecipeException {
-		Recipe r = new Recipe();
-		r.setName("Coffee");
-		assertEquals(true, r1.equals(r));
-	}
-
-
-
-	@Test
-	public void testReturnRecipeName() throws RecipeException {
-		when(recipeBookStub.getRecipes()).thenReturn(stubRecipies);
-		String rn = r1.toString();
-		assertEquals("Coffee", rn);
 	}
 
 
